6568250ebbf6c4ffdbd0e6a0fa0808aee1f389cf
Oier Bravo Urtasun <oierbravo@gmail.com>
2019-11-24 23:08:15 +0100

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp');
 var remoteSrc = require('gulp-remote-src');
 var concat = require('gulp-concat');
 
-const nunjucks = require('gulp-nunjucks-render');
+var package = require('./package.json');
+
+const nunjucks = require('gulp-nunjucks');
 const nunjucksMarkdown = require('nunjucks-markdown');
 const markdownIt = require('markdown-it');
 const gap = require('gulp-append-prepend');
@@ -55,12 +57,8 @@ function configMarkdown(env) {
 gulp.task('remote-md-full', function() {
      return remoteSrc(listaMds, {
             base: null
-        })
-        
+        })       
         .pipe(concat(config.mdFullFilename))
-        //.pipe(markdownPdf())
-        //.pipe(gap.prependText("{% extends 'html.html' %}"))
-        //.pipe(nunjucks({ path: 'templates', manageEnv: configMarkdown }))
         .pipe(gulp.dest(config.destination));
 })
 gulp.task('remote-md-items', function(cb) {
@@ -80,98 +78,7 @@ gulp.task('remote-md-items', function(cb) {
    };
    cb() ;
 });
-gulp.task('remote-md-items-concat', function() {
-  return gulp.src(['./dist/items/*.md'])
-     .pipe(concat(config.mdFullFilename))
-     .pipe(gulp.dest(config.destination));
-})
-gulp.task('remote-md-items-rehype', function() {
-  inlineStyle = "h1 {    font-size: 2em;  }  h2 {    font-size: 1.5em;  }  h3 {    font-size: 1.3em;  }  h4 {    font-size: 1.1em;  }  .A4{    width: 21cm;    font-size: 0.9em;  }td, th {text-align: left;}.h0Wrapper, .h3Wrapper, .h4Wrapper {page-break-inside :  avoid}  "
-  return gulp.src(['./dist/necrorol-full.md'])
-    .pipe(
-      remark()
-      .use(markdown)
-      
-      .use(normalizeHeadings)
-      //.use(slug)
-      //.use(sectionize)
-      .use(remark2rehype)
-    
-    .use(section)
-    .use(doc,{css:'gutenberg.css',style:inlineStyle})
-      .use(format)
-      //.use(html)
-      
-      
-      .use(html))
-      .pipe(rename({
-        basename: "index",
-        extname: ".html"
-      }))
-     .pipe(gulp.dest(config.destination));
-})
-gulp.task('remote-rehype', function() {
-  return remoteSrc(listaMds, {
-         base: null
-     })
-     //
-     .pipe(
-      remark()
-      .use(markdown)
-      
-      .use(normalizeHeadings)
-      //.use(slug)
-      //.use(sectionize)
-      .use(remark2rehype)
-     // .use(doc)
-     .use(section)
-      .use(format)
-      //.use(html)
-      
-      
-      .use(html))
 
-      .pipe(concat('rehype.html'))
-     .pipe(gulp.dest(config.destination));
-})
-gulp.task('remote-rehype-inject', function() {
-  return gulp.src(['templates/html.rehype.tmpl'])
-   
-     .pipe(fileinclude({
-       prefix: '@@',
-       basepath: '@file'
-     }))
-     .pipe(inlineCss())
-     .pipe(rename({
-      basename: "index",
-       extname: ".html"
-     }))
-      .pipe(gulp.dest(config.destination));
- })
-gulp.task('remote-rehype-full', function() {
-  return remoteSrc(listaMds, {
-         base: null
-     })
-     //
-     .pipe(
-      remark()
-      .use(markdown)
-      
-      .use(normalizeHeadings)
-      //.use(slug)
-      //.use(sectionize)
-      .use(remark2rehype)
-     // .use(doc)
-     .use(section)
-      .use(format)
-      //.use(html)
-      
-      
-      .use(html))
-
-      .pipe(concat('rehype.html'))
-     .pipe(gulp.dest(config.destination));
-})
 gulp.task('remote-remark', function() {
   return remoteSrc(listaMds, {
          base: null
@@ -184,15 +91,11 @@ gulp.task('remote-remark', function() {
       .use(htmlRemark))
     .pipe(gap.appendText('<div style="page-break-after: always;"></div>'))
     .pipe(cheerio(function ($, file) {
-      // Each file will be run through cheerio and each corresponding `$` will be passed here.
-      // `file` is the gulp file object
-      // Make all h1 tags uppercase
       var h1Id  = $('h1').attr('id');
       $('h1').parent().addClass('container h1');
       $('section h2').each(function () {
         var section = $(this);
         var h2Id = $(this).attr('id');
-        //$(this).attr('id','');
         section.parent().addClass('h2 part part-' + h2Id);
       });    
     }))
@@ -201,68 +104,38 @@ gulp.task('remote-remark', function() {
 })
 
 gulp.task('remote-remark-inject', function() {
- return gulp.src(['templates/html.tmpl'])
+ return gulp.src(['templates/necrorol.html'])
   
     .pipe(fileinclude({
       prefix: '@@',
       basepath: '@file'
     }))
-  //  .pipe(inlineCss())
     .pipe(rename({
-      basename: "index",
-      extname: ".html"
+      basename: "necrorol.v" + package.version
     }))
      .pipe(gulp.dest(config.destination));
 })
 gulp.task('remote-remark-pdf', function() {
-  return gulp.src('./dist/index.html')
-/*  .pipe(rename({
-    dirname: "",
-    extname: ".pdf"
-  }))
-    .pipe(html2pdf())*/
+  return gulp.src('./dist/necrorol.v' + package.version + '.html')
     .pipe(pdf({  format: "A4", orientation: "portrait"}))
     .pipe(rename({
-      basename: "necrorol",
-      //extname: ".pdf"
+      basename: "necrorol.v" + package.version
     }))
      .pipe(gulp.dest(config.destination));
 })
-gulp.task('pdf',function(){
-  return gulp.src(path.join(config.destination,config.mdFullFilename))
-     .pipe(markdownPdf({
-       remarkable: {
-         preset: 'full'
-       },
-       cssPath: './node_module/gutemberg-css/dist/gutenberg.css'
-      // cssPath:'./node_modules/bootstrap/dist/css/bootstrap.css'
-      // cssPath:'bootstrap.css'
-     }))
-     .pipe(gulp.dest(config.destination));
-})
-/*gulp.task('pdf',function(){
-  
-  return gulp.src(path.join(config.destination,config.mdFullFilename))
-     .pipe(nunjucks({ path: './templatse.html', manageEnv: configMarkdown }))
-     .pipe(gulp.dest(config.destination));
-})
-*/
+
 gulp.task('css',function(){
-  return gulp.src(['./node_modules/gutenberg-css/dist/gutenberg.css','templates/remark.css'])
+  return gulp.src(['./node_modules/gutenberg-css/dist/gutenberg.css','templates/styles.css','./node_modules/bootstrap/dist/css/bootstrap.css'])
 
      .pipe(gulp.dest(config.destination));
 })
 
+gulp.task('index',function(){
+  return gulp.src('templates/index.html')
+        .pipe(nunjucks.compile({version: package.version}))
+    .pipe(gulp.dest(config.destination));
+});
+gulp.task('remark', gulp.series(['remote-remark','remote-remark-inject','remote-remark-pdf']));
+gulp.task('build', gulp.series(['css','remark','index']));
 
 
-gulp.task('items', gulp.series(['css','remote-md-items','remote-md-items-concat','remote-md-items-rehype','remote-remark-pdf']));
-
-gulp.task('full', gulp.series(['css','remote-md-full','remote-rehype-full','remote-rehype-inject','remote-remark-pdf']));
-
-gulp.task('rehype', gulp.series(['css','remote-rehype','remote-rehype-inject','remote-remark-pdf']));
-
-gulp.task('remote', gulp.series(['remote-md-items','remote-md-full']));
-gulp.task('build', gulp.series(['remote','pdf']));
-
-
-gulp.task('remark', gulp.series(['css','remote-remark','remote-remark-inject','remote-remark-pdf']));
