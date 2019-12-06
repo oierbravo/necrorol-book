8c6b20e06c79876067449c0462f160bb0bcbe392
Oier Bravo Urtasun <oierbravo@gmail.com>
2019-12-06 10:31:20 +0100

    all the mds

--- lista-mds.js
+++ lista-mds.js
@@ -1,5 +1,12 @@
 module.exports = [
-    "https://owncloud.eneko.ovh/s/rZQc7MeS6WS7FAP/download",
-    "https://owncloud.eneko.ovh/s/icG2E5wXasEGb6P/download",
-    "https://owncloud.eneko.ovh/s/GYmnwzTDXTGexFC/download"
+    "https://owncloud.eneko.ovh/s/rZQc7MeS6WS7FAP/download",  //necrorol
+    "https://owncloud.eneko.ovh/s/icG2E5wXasEGb6P/download",  //habilidades comunes
+    "https://owncloud.eneko.ovh/s/GYmnwzTDXTGexFC/download",  //clases/apoyo
+    "https://owncloud.eneko.ovh/s/AoLYE62ZzaNWzpx/download",  //clases/ariete
+    "https://owncloud.eneko.ovh/s/X6XE8L5J7TEzARG/download",  //clases/explorador piel rata
+    "https://owncloud.eneko.ovh/s/3Ho5BZp5o94DoK3/download",  //clases/gatillo
+    "https://owncloud.eneko.ovh/s/6JYwPdHJpxi5zzr/download",  //clases/listillo
+    "https://owncloud.eneko.ovh/s/Ga8srBmzPKYaTo4/download",  //clases/pesado
+
+
 ]
