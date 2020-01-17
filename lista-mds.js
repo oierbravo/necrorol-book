09d4b1c0301110a6a9667fb47f40b91c112f7176
Eneko <enekonieto@gmail.com>
2020-01-17 19:28:27 +0100

    experiencia

--- lista-mds.js
+++ lista-mds.js
@@ -1,5 +1,6 @@
 module.exports = [
     "https://owncloud.eneko.ovh/s/rZQc7MeS6WS7FAP/download",  //necrorol
+    "https://owncloud.eneko.ovh/s/xqJWLc727KPknAp/download",  //experiencia
     "https://owncloud.eneko.ovh/s/icG2E5wXasEGb6P/download",  //habilidades comunes
     "https://owncloud.eneko.ovh/s/GYmnwzTDXTGexFC/download",  //clases/apoyo
     "https://owncloud.eneko.ovh/s/AoLYE62ZzaNWzpx/download",  //clases/ariete
@@ -7,6 +8,5 @@ module.exports = [
     "https://owncloud.eneko.ovh/s/3Ho5BZp5o94DoK3/download",  //clases/gatillo
     "https://owncloud.eneko.ovh/s/6JYwPdHJpxi5zzr/download",  //clases/listillo
     "https://owncloud.eneko.ovh/s/Ga8srBmzPKYaTo4/download",  //clases/pesado
-
 
 ]
